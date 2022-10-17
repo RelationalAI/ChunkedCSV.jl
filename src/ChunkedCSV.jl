5003bc4f1bb5b61b0adb19f18b35c39c4ca0eb1f
Tomáš Drvoštěp <tomas.drvostep@gmail.com>
2022-10-17 17:19:57 +0200

    Factor our parser setup logic

--- src/ChunkedCSV.jl
+++ src/ChunkedCSV.jl
@@ -100,6 +100,68 @@ function _create_options(delim::Char=',', quotechar::Char='"', escapechar::Char=
     )
 end
 
+function setup_parser(
+    input,
+    schema::Union{Nothing,Vector{DataType}}=nothing;
+    header::Union{Nothing,Vector{Symbol}}=nothing,
+    hasheader::Bool=true,
+    skiprows::Integer=UInt32(0),
+    limit::Integer=UInt32(0),
+    delim::Union{UInt8,Char}=',',
+    quotechar::Union{UInt8,Char}='"',
+    escapechar::Union{UInt8,Char}='"',
+    sentinel::Union{Missing,String,Vector{String}}=missing,
+    groupmark::Union{Char,UInt8,Nothing}=nothing,
+    stripwhitespace::Bool=false,
+    # In bytes. This absolutely has to be larger than any single row.
+    # Much safer if any two consecutive rows are smaller than this threshold.
+    buffersize::Integer=UInt32(Threads.nthreads() * 1024 * 1024),
+    nworkers::Integer=Threads.nthreads(),
+    maxtasks::Integer=2Threads.nthreads(),
+    nresults::Integer=maxtasks,
+    use_mmap::Bool=false,
+)
+    @assert 0 < buffersize < typemax(UInt32)
+    @assert skiprows >= 0
+    @assert limit >= 0
+    @assert nworkers > 0
+    @assert maxtasks >= nworkers
+    @assert nresults == maxtasks # otherwise not implemented; implementation postponed once we know why take!/wait allocates
+    !isnothing(header) && !isnothing(schema) && length(header) != length(schema) && error("Provided header doesn't match the number of column of schema ($(length(header)) names, $(length(schema)) types).")
+
+    should_close, io = _input_to_io(input, use_mmap)
+    settings = ParserSettings(schema, header, hasheader, Int(skiprows), UInt32(limit), UInt32(buffersize), UInt8(nworkers), UInt8(maxtasks), UInt8(nresults))
+    options = _create_options(delim, quotechar, escapechar, sentinel, groupmark, stripwhitespace)
+    byteset = Val(ByteSet((UInt8(options.e), UInt8(options.oq), UInt8('\n'), UInt8('\r'))))
+    (parsing_ctx, lexer_state) = init_parsing!(io, settings, options, Val(byteset))
+    options = _create_options(delim, quotechar, escapechar, sentinel, groupmark, stripwhitespace)
+    return should_close, parsing_ctx, lexer_state, options
+end
+
+function parse_file(
+    lexer_state::LexerState,
+    parsing_ctx::ParsingContext,
+    consume_ctx::AbstractConsumeContext,
+    options::Parsers.Options,
+    _force::Symbol=:none,
+)
+    @assert _force in (:none, :serial, :singlebuffer, :doublebuffer)
+    schema = parsing_ctx.schema
+    N = length(schema)
+    M = _bounding_flag_type(N)
+    if _force === :doublebuffer
+        _parse_file_doublebuffer(lexer_state, parsing_ctx, consume_ctx, options, Val(N), Val(M))::Nothing
+    elseif _force === :singlebuffer
+        _parse_file_singlebuffer(lexer_state, parsing_ctx, consume_ctx, options, Val(N), Val(M))::Nothing
+    elseif _force === :serial || Threads.nthreads() == 1 || parsing_ctx.nworkers == 1 || parsing_ctx.maxtasks == 1 || lexer_state.last_newline_at < MIN_TASK_SIZE_IN_BYTES
+              _parse_file_serial(lexer_state, parsing_ctx, consume_ctx, options, Val(N), Val(M))::Nothing
+    elseif !lexer_state.done
+        _parse_file_doublebuffer(lexer_state, parsing_ctx, consume_ctx, options, Val(N), Val(M))::Nothing
+    else
+        _parse_file_singlebuffer(lexer_state, parsing_ctx, consume_ctx, options, Val(N), Val(M))::Nothing
+    end
+end
+
 function parse_file(
     input,
     schema::Union{Nothing,Vector{DataType}}=nothing,
@@ -108,7 +170,6 @@ function parse_file(
     hasheader::Bool=true,
     skiprows::Integer=UInt32(0),
     limit::Integer=UInt32(0),
-    doublebuffer::Bool=true,
     delim::Union{UInt8,Char}=',',
     quotechar::Union{UInt8,Char}='"',
     escapechar::Union{UInt8,Char}='"',
@@ -124,36 +185,13 @@ function parse_file(
     _force::Symbol=:none,
     use_mmap::Bool=false,
 )
-    @assert 0 < buffersize < typemax(UInt32)
-    @assert skiprows >= 0
-    @assert limit >= 0
-    @assert nworkers > 0
-    @assert maxtasks >= nworkers
-    @assert nresults == maxtasks # otherwise not implemented; implementation postponed once we know why take!/wait allocates
-    @assert _force in (:none, :serial, :singlebuffer, :doublebuffer)
-    !isnothing(header) && !isnothing(schema) && length(header) != length(schema) && error("Provided header doesn't match the number of column of schema ($(length(header)) names, $(length(schema)) types).")
-
-    should_close, io = _input_to_io(input, use_mmap)
-    settings = ParserSettings(schema, header, hasheader, Int(skiprows), UInt32(limit), UInt32(buffersize), UInt8(nworkers), UInt8(maxtasks), UInt8(nresults))
-    options = _create_options(delim, quotechar, escapechar, sentinel, groupmark, stripwhitespace)
-    byteset = Val(ByteSet((UInt8(options.e), UInt8(options.oq), UInt8('\n'), UInt8('\r'))))
-    (parsing_ctx, lexer_state) = init_parsing!(io, settings, options, Val(byteset))
-    schema = parsing_ctx.schema
-    N = length(schema)
-    M = _bounding_flag_type(N)
-
-    if _force === :doublebuffer
-        _parse_file_doublebuffer(lexer_state, parsing_ctx, consume_ctx, options, Val(N), Val(M))::Nothing
-    elseif _force === :singlebuffer
-        _parse_file_singlebuffer(lexer_state, parsing_ctx, consume_ctx, options, Val(N), Val(M))::Nothing
-    elseif _force === :serial || Threads.nthreads() == 1 || nworkers == 1 || maxtasks == 1 || lexer_state.last_newline_at < MIN_TASK_SIZE_IN_BYTES
-              _parse_file_serial(lexer_state, parsing_ctx, consume_ctx, options, Val(N), Val(M))::Nothing
-    elseif doublebuffer && !lexer_state.done
-        _parse_file_doublebuffer(lexer_state, parsing_ctx, consume_ctx, options, Val(N), Val(M))::Nothing
-    else
-        _parse_file_singlebuffer(lexer_state, parsing_ctx, consume_ctx, options, Val(N), Val(M))::Nothing
-    end
-    should_close && close(io)
+    (should_close, parsing_ctx, lexer_state, options) = setup_parser(
+        input, schema; 
+        header, hasheader, skiprows, delim, quotechar, limit, escapechar, sentinel, groupmark, stripwhitespace, 
+        buffersize, nworkers, maxtasks, nresults, use_mmap
+    )
+    parse_file(lexer_state, parsing_ctx, consume_ctx, options, _force)
+    should_close && close(lexer_state.io)
     return nothing
 end
 

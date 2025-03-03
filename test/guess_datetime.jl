df8ba4e7c3a84cb9985a9467befae383797766c9
Tomáš Drvoštěp <tomas.drvostep@gmail.com>
2025-03-03 13:58:35 +0100

    .

--- test/guess_datetime.jl
+++ test/guess_datetime.jl
@@ -6,24 +6,45 @@ using Test
 const DT = b"1969-07-20 20:17:00"
 
 macro test_noalloc(e) :(@test(@allocated($(esc(e))) == 0)) end
-function test_utc_equiv(input, expected)
-    res = Parsers.xparse(ChunkedCSV.GuessDateTime, input, 1, length(input), Parsers.OPTIONS, DateTime)
-    @test res.val == expected
-    @test Parsers.ok(res.code)
-    @test_noalloc Parsers.xparse(ChunkedCSV.GuessDateTime, input, 1, length(input), Parsers.OPTIONS, DateTime)
-    return nothing
-end
+
 @testset "GuessDateTime" begin
     @testset "Parsing UTC equivalent timezones does not allocate" begin
         for tz in (b"-00", b"+00", b"-0000", b"+0000", b"-00:00", b"+00:00", b"UTC", b"GMT")
             dt = vcat(DT, tz)
-            @testset "$(String(copy(tz)))" begin
-                test_utc_equiv(dt, DateTime(1969, 7, 20, 20, 17))
+            @testset "$tz" begin
+                res = Parsers.xparse(ChunkedCSV.GuessDateTime, dt, 1, length(dt), Parsers.OPTIONS, DateTime)
+                @test res.val == DateTime(1969, 7, 20, 20, 17)
+                @test Parsers.ok(res.code)
+                @test_noalloc Parsers.xparse(ChunkedCSV.GuessDateTime, dt, 1, length(dt), Parsers.OPTIONS, DateTime)
             end
         end
     end
 
     @testset "Datetimes with only the Date part" begin
+        res = Parsers.xparse(ChunkedCSV.GuessDateTime, "0-01-01")
+        @test res.val == DateTime(0, 1, 1)
+        @test Parsers.ok(res.code)
+
+        res = Parsers.xparse(ChunkedCSV.GuessDateTime, "-0-01-01")
+        @test res.val == DateTime(0, 1, 1)
+        @test Parsers.ok(res.code)
+
+        res = Parsers.xparse(ChunkedCSV.GuessDateTime, "1-01-01")
+        @test res.val == DateTime(1, 1, 1)
+        @test Parsers.ok(res.code)
+
+        res = Parsers.xparse(ChunkedCSV.GuessDateTime, "-1-01-01")
+        @test res.val == DateTime(-1, 1, 1)
+        @test Parsers.ok(res.code)
+
+        res = Parsers.xparse(ChunkedCSV.GuessDateTime, "10-01-01")
+        @test res.val == DateTime(10, 1, 1)
+        @test Parsers.ok(res.code)
+
+        res = Parsers.xparse(ChunkedCSV.GuessDateTime, "-10-01-01")
+        @test res.val == DateTime(-10, 1, 1)
+        @test Parsers.ok(res.code)
+
         res = Parsers.xparse(ChunkedCSV.GuessDateTime, "-001-01-01")
         @test res.val == DateTime(-1, 1, 1)
         @test Parsers.ok(res.code)
